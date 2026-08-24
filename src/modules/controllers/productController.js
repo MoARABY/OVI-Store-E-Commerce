6d6647bb20528a45a94aa1e7d07e5dc8c7659fcb
MoARABY <arabym702@gmail.com>
2026-08-24 16:22:45 +0300

    feat: redesign products apis response and add seed data by script

--- src/modules/controllers/productController.js
+++ src/modules/controllers/productController.js
@@ -7,13 +7,13 @@ const asyncHandler = require('express-async-handler')
 
 const createProduct = asyncHandler(async(req,res)=>{ 
     const product = await productModel.create(req.body)
-    product ? res.status(201).json({msg:'product created succesffully',product}) : res.status(400).json('can not create product')
+    product ? res.status(201).json({status:'success',message:'product created succesffuly',data:product}) : res.status(400).json({status:'fail',message:'cannot create product'})
 })
 
 const getProduct = asyncHandler(async(req,res)=>{
     const {id} = req.params
     const product = await productModel.findById(id)
-    product ? res.status(200).json(product) : res.status(400).json('cannot find product')
+    product ? res.status(200).json({status:'success',message:'product found',data:product}) : res.status(400).json({status:'fail',message:'cannot find product'})
 })
 
 const getProducts = asyncHandler(async(req,res)=>{
@@ -30,19 +30,19 @@ const getProducts = asyncHandler(async(req,res)=>{
     const {mongooseQuery,paginateFeatures } = apiFeatures
 
     const products = await mongooseQuery
-    products ? res.status(200).json({length:products.length,paginateFeatures,Data:products}) : res.status(400).json('cannot find products')
+    products ? res.status(200).json({status:'success',message:'products found',length:products.length,paginateFeatures,Data:products}) : res.status(400).json({status:'fail',message:'cannot find products'})
 })
 
 const updateProduct = asyncHandler(async(req,res)=>{
     const {id} = req.params
     const product = await productModel.findByIdAndUpdate(id,req.body,{new:true})
-    product ? res.status(200).json({msg:'product updated succesffuly',product}) : res.status(400).json('cannot find product')
+    product ? res.status(200).json({status:'success',message:'product updated succesffuly',data:product}) : res.status(400).json({status:'fail',message:'cannot find product'})
 })
 
 const deleteProduct = asyncHandler(async(req,res)=>{
     const {id} = req.params
     const product = await productModel.findByIdAndDelete(id,req.body,{new:true})
-    product ? res.status(200).json({msg:'product deleted succesffuly'}) : res.status(400).json('cannot find product')
+    product ? res.status(200).json({status:'success',message:'product deleted succesffuly'}) : res.status(400).json({status:'fail',message:'cannot find product'})
 })
 
 module.exports = {createProduct,getProduct,getProducts,updateProduct,deleteProduct}
